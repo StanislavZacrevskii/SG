5181a08e2c9213af35b55854e0ca5f186aca432e
Stanislav <szakrevskii@intelverse.net>
2019-06-23 18:26:39 +0300

    Add Profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Defaults} from "../../shared/settings/defaults";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+    public regexp = Defaults.regexp;
+    public currentUserAva: string = "http://placekitten.com/g/100/100";
+    public createProfileForm: FormGroup;
 
-  ngOnInit() {
-  }
 
+    constructor(private formBuilder: FormBuilder,
+                private router: Router) { }
+
+    ngOnInit() {
+        this.createProfileCreateForm();
+        this.setProfileInfo();
+    }
+
+    createProfileCreateForm() {
+        this.createProfileForm = this.formBuilder.group({
+            'name': ['', [Validators.required]],
+            'email': [{value: '', disabled: true}, [Validators.required, Validators.email]],
+            'telephone': ['', [Validators.required, Validators.pattern(this.regexp.telephoneRegexp)]],
+        })
+    }
+
+    setProfileInfo() {
+        this.createProfileForm.setValue({
+            name: 'Johan',
+            email: 'test@gmail.com',
+            telephone: '0976880625'
+        })
+    }
+
+    saveProfile() {
+        setTimeout(() => {
+            this.goToHomePage();
+        },2000)
+    }
+
+    goToHomePage() {
+        this.router.navigate(['/']);
+    }
 }
--- src/app/shared/settings/defaults.ts
+++ src/app/shared/settings/defaults.ts
@@ -3,5 +3,10 @@ export class Defaults {
     public static limit = {
         titleRecordMax: 255,
         recordDescMax: 20000
-    }
+    };
+
+    public static regexp = {
+        telephoneRegexp: new RegExp("^\\d+$"),
+    };
+
 }
